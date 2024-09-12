e14040476e096493e780b49ac9ef3e90731588f2
Naiara Santos <127265827+NaiaraSantos3@users.noreply.github.com>
2024-09-11 21:41:38 -0300

    Add files via upload

---
+++ Springbootlab4-2024/src/main/java/naiara/santos3/Springbootlab4_2024/Springbootlab42024Application.java
@@ -0,0 +1,13 @@
+package naiara.santos3.Springbootlab4_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootlab42024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootlab42024Application.class, args);
+	}
+
+}
---
+++ Springbootlab4-2024/src/test/java/naiara/santos3/Springbootlab4_2024/Springbootlab42024ApplicationTests.java
@@ -0,0 +1,13 @@
+package naiara.santos3.Springbootlab4_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootlab42024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
